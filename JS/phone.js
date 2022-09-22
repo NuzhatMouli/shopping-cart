2da068bef305aa35b84badd65aa4a1c4a2e4a01f
NuzhatMouli <moulictg09@gmail.com>
2022-09-22 13:01:31 +0600

    first commit

---
+++ JS/phone.js
@@ -0,0 +1,7 @@
+document.getElementById('btn-phn-plus').addEventListener('click',function(){
+    const phoneNumberField = document.getElementById('phone-number-field');
+    const phoneNumberString = phoneNumberField.value;
+    const previousPhoneNumber = parseInt(phoneNumberString);
+    const newPhoneNumber = previousPhoneNumber + 1;
+    phoneNumberField.value = newPhoneNumber;
+})
